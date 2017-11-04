470008022854f5b3ad912427aa1197a32fd68f67
Omar Elazhary <omazhary@gmail.com>
2017-11-04 15:06:13 -0700

    Adds environment port to app.py

--- app.py
+++ app.py
@@ -31,4 +31,5 @@ def generate():
         return json.dumps({'error': 'You need at least 3 participants for this to be any fun!!'})
 
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
