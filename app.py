6e626b0dda682e230946cbb50f899ef24cd16775
Omar Elazhary <omazhary@gmail.com>
2017-11-04 12:31:50 -0700

    Adds generation method

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, json
 app = Flask(__name__)
 
 
 @app.route("/")
 def main():
     return render_template('index.html')
+
+@app.route("/generate", methods=['POST'])
+def generate():
+    _participants = request.form['participants']
+    if _participants:
+        return json.dumps({'html':'<span>Field good!</span>'})
+    else:
+        return json.dumps({'html':'<span>Field bad!</span>'})
 
 if __name__ == "__main__":
     app.run()
